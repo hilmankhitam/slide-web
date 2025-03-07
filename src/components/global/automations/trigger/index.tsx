09f71a9a7d64403280ca1bf745485b84140c5a69
hilman khitam <hilmankhitam100@gmail.com>
2025-03-07 09:22:05 +0800

    setting up triggers for automations

--- src/components/global/automations/trigger/index.tsx
+++ src/components/global/automations/trigger/index.tsx
@@ -1,13 +1,57 @@
-import React from 'react'
+"use client";
+
+import { useQueryAutomation } from "@/hooks/use-queries";
+import React from "react";
+import ActiveTrigger from "./active";
+import { Separator } from "@/components/ui/separator";
 
 type Props = {
-    id: string
-}
+  id: string;
+};
 
-const Trigger = ({id}: Props) => {
+const Trigger = ({ id }: Props) => {
+  const { data } = useQueryAutomation(id);
+
+  // if(data?.data && data?.data?.trigger.length > 0){
   return (
-    <div>Trigger</div>
-  )
-}
+    <div className="flex flex-col gap-y-6 items-center">
+      {/* WIP: add this for the type data.data.trigger[0].type */}
+      <ActiveTrigger
+        type="COMMENT"
+        keywords={[
+          {
+            id: "asdasdasdasd",
+            word: "getstarted",
+            automationId: id,
+          },
+        ]}
+      />
+      {/* WIP: add this for the type data.data.trigger.length */}
+      {/* {data?.data?.trigger.length > 1 && <></>} */}
+      <>
+        <div className="relative w-6/12 mt-4">
+          <p className="absolute transform px-2 -translate-y-1/2 top-1/2 -translate-x-1/2 left-1/2">
+            or
+          </p>
+          <Separator
+            orientation="horizontal"
+            className="border-muted border-[1px]"
+          />
+        </div>
+        <ActiveTrigger
+          type="MESSAGE"
+          keywords={[
+            {
+              id: "asdasdasdasd",
+              word: "Meplease",
+              automationId: id,
+            },
+          ]}
+        />
+      </>
+    </div>
+  );
+  // }
+};
 
-export default Trigger
+export default Trigger;
